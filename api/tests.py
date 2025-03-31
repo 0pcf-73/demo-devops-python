b608f4e7086cf2749d0266b809ea16bfc46a8570
0pcf-73 <ypaloma.carmonaf@gmail.com>
2025-03-31 18:38:03 -0500

    Update tests.py

--- api/tests.py
+++ api/tests.py
@@ -13,7 +13,16 @@ class TestUserView(APITestCase):
     def test_post(self):
         response = self.client.post(self.url, self.data, format='json')
         self.assertEqual(response.status_code, 201)
-        self.assertEqual(json.loads(response.content), {"id": 2, "name":"Test2", "dni":"09876543211"})
+        
+        data = json.loads(response.content)
+        # Verifica que 'dni' y 'name' sean los esperados
+        self.assertEqual(data.get('dni'), "09876543211")
+        self.assertEqual(data.get('name'), "Test2")
+        # Verifica que 'id' exista y sea un número entero
+        self.assertIn('id', data)
+        self.assertIsInstance(data['id'], int)
+
+        # Además, comprueba que se haya creado el usuario (total 2 en la base de datos)
         self.assertEqual(User.objects.count(), 2)
 
     def test_get_list(self):
@@ -24,4 +33,4 @@ class TestUserView(APITestCase):
     def test_get(self):
         response = self.client.get(self.url + '1/')
         self.assertEqual(response.status_code, 200)
-        self.assertEqual(json.loads(response.content), {"id": 1, "name":"Test1", "dni":"09876543210"})
+        self.assertEqual(json.loads(response.content), {"id": 1, "name": "Test1", "dni": "09876543210"})
