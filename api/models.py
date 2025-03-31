d3d08fb6e653b80ced33f6a373573f933e29ba67
Paloma Carmona <ypaloma_carmonaf@gmail.com>
2025-03-31 06:59:54 -0500

    Fix: Corregir basename en router DRF

--- api/models.py
+++ api/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 class User(models.Model):
     dni = models.CharField(max_length=13, unique=True)
--- api/urls.py
+++ api/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from rest_framework import routers
 
 router = routers.DefaultRouter()
-router.register('users', UserViewSet, 'users')
+router.register(r'users', UserViewSet, basename='users')
+
 
 urlpatterns = router.urls
