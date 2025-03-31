64b4434ee9b63cea2fc62dec47fe454e1d83260f
Paloma Carmona <ypaloma_carmonaf@gmail.com>
2025-03-31 10:33:14 -0500

    correcion de configuración de database

--- demo/settings.py
+++ demo/settings.py
@@ -84,7 +84,7 @@ WSGI_APPLICATION = 'demo.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config(default=os.getenv("DATABASE_URL", "sqlite:///db.sqlite3"))
+    'default': dj_database_url.config(default=f'sqlite:///{BASE_DIR}/db.sqlite3')
     }
 
 
